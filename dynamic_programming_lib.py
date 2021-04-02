2866a34622faee9c790fa7fa454311a3621fb7f1
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2021-04-02 10:18:46 +0200

    Edited dynamic_programming.

--- dynamic_programming_lib.py
+++ dynamic_programming_lib.py
@@ -8,44 +8,51 @@ Date: March 31, 2021
 # Import necessary modules.
 import numpy as np
 
-def policy_eval(P, R, gamma, pi, v_pi, tol=1.0e-6):
+def policy_eval(P, R, gamma, pi, v_init, tol=1.0e-6):
     """Iteratively estimates the value of a given policy, starting from v."""
-    # Build transition probability and reward matrices for the given policy.
-    v = np.copy(v_pi)
+    # Number of states.
     S = P.shape[0]
+    # Transition matrix.
     P_pi = np.zeros((S, S))
+    # Expected rewards.
     R_pi = np.zeros(S)
+    # Build transition probability and reward matrices for the given policy.
     for s in range(S):
         P_pi[s, :] = np.copy(P[s, :, pi[s]])
         R_pi[s] = np.copy(R[s, pi[s]])
     # Policy evaluation loop.
+    v = np.copy(v_init)
     while True:
         prev_v = np.copy(v)
-        v = R_pi + gamma*np.dot(P_pi, v)
+        v = R_pi + gamma*np.dot(P_pi, prev_v)
         if np.linalg.norm(v - prev_v) < tol:
             break
     return v
 
-def policy_improv(P, R, gamma, v):
+def policy_improv(P, R, gamma, v_pi):
     """Returns an improved policy from a given value."""
-    # Initialize new policy vector.
+    # Number of states.
     S = P.shape[0]
+    # Number of actions.
     A = R.shape[1]
+    # Initialize new policy vector.
     pi = np.zeros(S)
     # Policy improvement loop.
     q = np.zeros(A)
     for s in range(S):
-        q *= 0.0
+        q.fill(0.0)
         for a in range(A):
             p_trans = P[s, :, a]
-            q[a] = R[s, a] + gamma*np.dot(p_trans, v)
+            q[a] = R[s, a] + gamma*np.dot(p_trans, v_pi)
         pi[s] = np.argmax(q)
     return pi
 
 def policy_iteration(P, R, gamma, v_init, pi_init, tol=1.0e-6):
     """Merges policy evaluation and improvement."""
-    v = np.copy(v_init)
+    # Initial estimate of the policy.
     prev_pi = np.copy(pi_init)
+    # Initial estimate of the policy value.
+    v = np.copy(v_init)
     # Policy iteration loops.
     while True:
         v = policy_eval(P, R, gamma, prev_pi, v, tol)
@@ -53,29 +60,32 @@ def policy_iteration(P, R, gamma, v_init, pi_init, tol=1.0e-6):
         if np.linalg.norm(pi - prev_pi, ord=np.inf) == 0.0:
             break
         else:
-            prev_pi = pi
+            prev_pi = np.copy(pi)
     return pi, v
 
 def value_iteration(P, R, gamma, v_init, tol=1.0e-6):
     """Returns the optimal policy and its value, from a given value."""
-    # Initialize policy and value vectors.
+    # Number of states.
     S = P.shape[0]
+    # Number of actions.
     A = R.shape[1]
-    v_pi = np.zeros(S)
-    q_pi = np.zeros(A)
+    # Initialize policy vector.
     pi = np.zeros(S)
-    prev_v = np.copy(v_init)
+    # Initialize policy value vectors.
+    v = np.zeros(S)
+    q = np.zeros(A)
     # Value iteration loop.
+    prev_v = np.copy(v_init)
     while True:
         for s in range(S):
-            q_pi *= 0.0
+            q.fill(0.0)
             for a in range(A):
                 p_trans = P[s, :, a]
-                q_pi[a] = R[s, a] + gamma*np.dot(p_trans, prev_v)
-            pi[s] = np.argmax(q_pi)
-            v_pi[s] = q_pi[pi[s]]
-        if np.linalg.norm(v_pi - prev_v) < tol:
+                q[a] = R[s, a] + gamma*np.dot(p_trans, prev_v)
+            pi[s] = np.argmax(q)
+            v[s] = q[pi[s]]
+        if np.linalg.norm(v - prev_v) < tol:
             break
         else:
-            prev_v = np.copy(v_pi)
-    return pi, v_pi
+            prev_v = np.copy(v)
+    return pi, v
