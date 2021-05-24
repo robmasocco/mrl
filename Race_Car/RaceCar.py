b35bcf3c198d3d5ec32ea94c544d9620d2c0ca5d
robmasocco <robmasocco@gmail.com>
2021-05-24 12:45:02 +0200

    Debugged the world.

--- Race_Car/RaceCar.py
+++ Race_Car/RaceCar.py
@@ -1,4 +1,3 @@
-
 """
     RACE CAR
 """
@@ -6,17 +5,19 @@
 import numpy as np
 import pygame
 
-
 class RaceCar:
     """
         Fuck autopep8
     """
 
-    def __init__(self, race_map, init_coords, pi, epsilon, width, height):
+    def __init__(self, race_map, init_coords, pi, epsilon, width, height, actions_x, actions_y):
         self.map = race_map
         self.coords = init_coords
+        self.actions_x = actions_x
+        self.actions_y = actions_y
         self.set_state()
         print(self.state)
+        print(self.map_state())
         self.pi = pi
         self.epsilon = epsilon
         self.states = np.empty(0, dtype=np.int32)
@@ -34,13 +35,30 @@ class RaceCar:
         return np.asarray(np.unravel_index(self.state, self.map.shape))
 
     def map_action(self, action):
-        return np.asarray(np.unravel_index(action, (11, 6))) - [5, 0]
+        return np.asarray(np.unravel_index(action, (self.actions_x, self.actions_y)))
 
     def drive(self, velocity):
-        self.coords[0] = np.amax(
-            [np.amin([self.coords[0] + velocity[0], self.map.shape[0] - 1]), 0])
-        self.coords[1] = np.amax(
-            [np.amin([self.coords[1] + velocity[1], self.map.shape[1] - 1]), 0])
+        """TODO: Updates X before Y checking path. Shit is reversed."""
+        # Loop over X coordinates updates.
+        temp_x = 0
+        for x in range(velocity[0] + 1):
+            temp_x = np.amax([np.amin([self.coords[1] + x, self.map.shape[1] - 1]), 0])
+            if self.map[self.coords[0], temp_x] == -1:
+                # Hit obstacle.
+                self.coords[1] = temp_x
+                return
+        # Hit no obstacle: update coordinates.
+        self.coords[1] = temp_x
+        # Loop over Y coordinates updates.
+        temp_y = 0
+        for y in range(velocity[1] + 1):
+            temp_y = np.amax([np.amin([self.coords[0] - y, self.map.shape[0] - 1]), 0])
+            if self.map[temp_y, self.coords[1]] == -1:
+                # Hit obstacle.
+                self.coords[0] = temp_y
+                return
+        # Hit no obstacle: update coordinates.
+        self.coords[0] = temp_y
 
     def run(self):
         self.draw_map()
@@ -51,14 +69,14 @@ class RaceCar:
             if np.random.choice([0, 1], p=[1.0 - self.epsilon, self.epsilon]) == 0:
                 action = self.pi[self.state]
             else:
-                action = np.random.randint(66)
+                action = np.random.randint(self.actions_x * self.actions_y)
             self.actions = np.append(self.actions, action)
             velocity = self.map_action(action)
+            print("Action: {}, {}.".format(action, velocity))
             self.drive(velocity)
             if self.map[self.coords[0], self.coords[1]] == -1:
                 self.state = -1
-                self.rewards = np.append(
-                    self.rewards, -self.map.shape[0] * self.map.shape[1])
+                self.rewards = np.append(self.rewards, -self.map.shape[0] * self.map.shape[1])
             elif self.map[self.coords[0], self.coords[1]] == 2:
                 self.state = -1
                 self.rewards = np.append(self.rewards, 0)
@@ -67,6 +85,7 @@ class RaceCar:
                 self.rewards = np.append(self.rewards, -1)
             self.draw_map()
             pygame.display.update()
+            self.clock.tick(1)
         pygame.quit()
 
     def get_states(self):
